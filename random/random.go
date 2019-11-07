753ed4779110f65bc7cd2698c5c235ec232dc7d9
zxfonline <zxfonline@sina.com>
2019-11-07 21:00:32 +0800

    优化

--- random/random.go
+++ random/random.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func RandInt32(min int32, max int32) int32 {
+func RandInt32(min int32, max int32, randz *rand.Rand) int32 {
 	if max <= min {
 		max = min + 1
 	}
@@ -18,9 +18,9 @@ func RandInt32(min int32, max int32) int32 {
 		min += base
 		max += base
 	}
-	return -base + min + rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(max-min)
+	return -base + min + randz.Int31n(max-min)
 }
-func RandInt64(min int64, max int64) int64 {
+func RandInt64(min int64, max int64, randz *rand.Rand) int64 {
 	if max <= min {
 		max = min + 1
 	}
@@ -30,9 +30,9 @@ func RandInt64(min int64, max int64) int64 {
 		min += base
 		max += base
 	}
-	return -base + min + rand.New(rand.NewSource(time.Now().UnixNano())).Int63n(max-min)
+	return -base + min + randz.Int63n(max-min)
 }
-func RandInt(min int, max int) int {
+func RandInt(min int, max int, randz *rand.Rand) int {
 	if max <= min {
 		max = min + 1
 	}
@@ -42,7 +42,7 @@ func RandInt(min int, max int) int {
 		min += base
 		max += base
 	}
-	return -base + min + rand.New(rand.NewSource(time.Now().UnixNano())).Intn(max-min)
+	return -base + min + randz.Intn(max-min)
 }
 
 //func main() {
@@ -64,7 +64,7 @@ func RandInt(min int, max int) int {
  * @param n 要随机多n个数
  * @return
  */
-func GetRandomValues(numbers []int, n int) []int {
+func GetRandomValues(numbers []int, n int, radom *rand.Rand) []int {
 	size := len(numbers)
 	filter := make([]int, size)
 	copy(filter, numbers)
@@ -73,7 +73,7 @@ func GetRandomValues(numbers []int, n int) []int {
 	}
 	list := make([]int, 0, n)
 	for i := 0; i < n; i++ {
-		index := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(filter))
+		index := radom.Intn(len(filter))
 		list = append(list, filter[index])
 		filter = append(filter[:index], filter[index+1:]...)
 	}
@@ -87,7 +87,7 @@ func GetRandomValues(numbers []int, n int) []int {
  * @param n 要随机多n个数
  * @return
  */
-func GetRandomValuesInt64(numbers []int64, n int) []int64 {
+func GetRandomValuesInt64(numbers []int64, n int, randz *rand.Rand) []int64 {
 	size := len(numbers)
 	filter := make([]int64, size)
 	copy(filter, numbers)
@@ -96,7 +96,7 @@ func GetRandomValuesInt64(numbers []int64, n int) []int64 {
 	}
 	list := make([]int64, 0, n)
 	for i := 0; i < n; i++ {
-		index := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(len(filter))
+		index := randz.Intn(len(filter))
 		list = append(list, filter[index])
 		filter = append(filter[:index], filter[index+1:]...)
 	}
@@ -111,13 +111,13 @@ func GetRandomValuesInt64(numbers []int64, n int) []int64 {
  * @see #GetRandomNumber(String, Random)
  * @return
  */
-func GetRandomNumbers(args string, radom *rand.Rand) []int {
+func GetRandomNumbers(args string, randz *rand.Rand) []int {
 	strs := strings.Split(args, "#")
 	size := len(strs)
 	ints := make([]int, 0, size)
 	for i := 0; i < size; i++ {
 		if len(strs[i]) > 0 {
-			ints = append(ints, GetRandomNumber(strs[i], 1, radom)[0])
+			ints = append(ints, GetRandomNumber(strs[i], 1, randz)[0])
 		}
 	}
 	return ints
@@ -136,9 +136,9 @@ func GetRandomNumbers(args string, radom *rand.Rand) []int {
  * @param radom
  * @return
  */
-func GetRandomNumber(args string, n int, radom *rand.Rand) []int {
-	if radom == nil {
-		radom = rand.New(rand.NewSource(time.Now().UnixNano()))
+func GetRandomNumber(args string, n int, randz *rand.Rand) []int {
+	if randz == nil {
+		randz = rand.New(rand.NewSource(time.Now().UnixNano()))
 	}
 	var err error
 	//	if strings.Index(args, ",") > 0 {
@@ -166,14 +166,14 @@ func GetRandomNumber(args string, n int, radom *rand.Rand) []int {
 				weights[i] = value
 				weightSum += value
 			}
-			if numbers[i], err = average(valuesStr, radom); err != nil {
+			if numbers[i], err = average(valuesStr, randz); err != nil {
 				panic(fmt.Errorf("invalid args:%v,err:%v", args, err))
 			}
 		}
 		// 随机多个
 		rd := make([]int, 0, n)
 		for j := 0; j < n; j++ {
-			ranNum := radom.Intn(weightSum)
+			ranNum := randz.Intn(weightSum)
 			for i := 0; i < size; i++ {
 				ranNum -= weights[i]
 				if ranNum < 0 {
@@ -189,14 +189,14 @@ func GetRandomNumber(args string, n int, radom *rand.Rand) []int {
 		return rd
 	} else { // 1~10,44~89,2~5
 		for i := 0; i < size; i++ {
-			if numbers[i], err = average(values[i], radom); err != nil {
+			if numbers[i], err = average(values[i], randz); err != nil {
 				panic(fmt.Errorf("invalid args:%v,err:%v", args, err))
 			}
 		}
 		if n == 1 {
-			return []int{numbers[radom.Intn(size)]}
+			return []int{numbers[randz.Intn(size)]}
 		} else {
-			return GetRandomValues(numbers, n)
+			return GetRandomValues(numbers, n, randz)
 		}
 	}
 }
@@ -223,9 +223,9 @@ type RandItem struct {
 	Weight  int32 `json:"weight"`  //权重
 }
 
-func GetRandomItems(items []RandItem, n int, radom *rand.Rand) []RandItem {
-	if radom == nil {
-		radom = rand.New(rand.NewSource(time.Now().UnixNano()))
+func GetRandomItems(items []RandItem, n int, randz *rand.Rand) []RandItem {
+	if randz == nil {
+		randz = rand.New(rand.NewSource(time.Now().UnixNano()))
 	}
 	size := len(items)
 	if n > size {
@@ -237,7 +237,7 @@ func GetRandomItems(items []RandItem, n int, radom *rand.Rand) []RandItem {
 	}
 	rd := make([]RandItem, 0, n)
 	for j := 0; j < n; j++ {
-		ranNum := radom.Intn(weightSum)
+		ranNum := randz.Intn(weightSum)
 		for i := 0; i < size; i++ {
 			ranNum -= int(items[i].Weight)
 			if ranNum < 0 {
@@ -261,9 +261,9 @@ type RandInterface interface {
 }
 
 //GetRandomWeight 随机数选取
-func GetRandomWeight(data RandInterface, n int, radom *rand.Rand) interface{} {
-	if radom == nil {
-		radom = rand.New(rand.NewSource(time.Now().UnixNano()))
+func GetRandomWeight(data RandInterface, n int, randz *rand.Rand) interface{} {
+	if randz == nil {
+		randz = rand.New(rand.NewSource(time.Now().UnixNano()))
 	}
 	size := data.Len()
 	if n > size {
@@ -277,7 +277,7 @@ func GetRandomWeight(data RandInterface, n int, radom *rand.Rand) interface{} {
 	var indexs []int
 
 	for j := 0; j < n; j++ {
-		ranNum := radom.Intn(weightSum)
+		ranNum := randz.Intn(weightSum)
 		for i := 0; i < size; i++ {
 			find := false
 			for _, index := range indexs {
